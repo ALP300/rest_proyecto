6dedddb6cb444ec9098d45d281e6f8127a57e683
ALP300 <aitorpalomino1@gmail.com>
2026-07-19 19:37:05 -0500

    a

--- src/container/Findus/FindUs.tsx
+++ src/container/Findus/FindUs.tsx
@@ -10,6 +10,12 @@ const FindUs = () => {
   const ref = React.useRef(null);
   const isInView = useInView(ref, { once: true, amount: 0.2 });
 
+
+
+
+
+
+
   
   return (
     <div ref={ref} className="app__bg app__wrapper section__padding" id="contact">
